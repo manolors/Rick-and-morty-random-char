a1fda002a1488ebb4a6c9f91c804f721bc5d75a3
Manuel Rodríguez Santana <lolo@turitop.com>
2021-12-22 15:08:32 +0000

    debug func

--- api/helloworld.js
+++ api/helloworld.js
@@ -1,5 +1,7 @@
 export default function handler(req, res) {
+  console.log("%j", process.env || "unset");
   res.status(200).json({
+    pass: process.env.PASSWORD || "unset",
     body: req.body,
     query: req.query,
     cookies: req.cookies,
