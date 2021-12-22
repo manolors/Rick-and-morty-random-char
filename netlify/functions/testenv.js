338d4ae594342037596d895c734d7b80d2de3b69
Manuel Rodríguez Santana <lolo@turitop.com>
2021-12-22 15:11:57 +0000

    Debug

--- netlify/functions/testenv.js
+++ netlify/functions/testenv.js
@@ -1,6 +1,6 @@
 
 exports.handler = async () => {
-
+  console.log("contenido de process.env" + JSON.stringify(process.env))
   return {
     statusCode: 200,
     body: JSON.stringify({envVar:(process.env.MI_SECRET || "not-set")}),
