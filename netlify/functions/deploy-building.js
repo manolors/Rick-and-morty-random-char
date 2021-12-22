98987edfcac33731c9c776772756ab2d59621c8b
Manuel Rodríguez Santana <lolo@turitop.com>
2021-12-22 13:27:52 +0000

    Add function triggers

---
+++ netlify/functions/deploy-building.js
@@ -0,0 +1,28 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const webhookEndpoint = "https://webhook.site/ae9798bc-5609-4443-aed5-bfb59520dbfe"
+const handler = async (event) => {
+  try {
+    sendMessage(webhookEndpoint, "Deploy building")
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+function sendMessage(endpoint, message) {
+  const request = new XMLHttpRequest();
+  request.open("POST", endpoint);
+
+  request.setRequestHeader('Content-type', 'application/json');
+
+  const params = {
+    content: message
+  }
+
+  request.send(JSON.stringify(params));
+}
+
+module.exports = { handler }
--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,28 +1,25 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const webhookEndpoint = "https://webhook.site/ae9798bc-5609-4443-aed5-bfb59520dbfe"
 const handler = async (event) => {
   try {
-    sendMessage()
+    sendMessage(webhookEndpoint, "Deploy succeded!")
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
-function sendMessage() {
+function sendMessage(endpoint, message) {
   const request = new XMLHttpRequest();
-  request.open("POST", "https://webhook.site/ae9798bc-5609-4443-aed5-bfb59520dbfe");
+  request.open("POST", endpoint);
 
   request.setRequestHeader('Content-type', 'application/json');
 
   const params = {
-    username: "My Webhook Name",
-    content: "The message to send"
+    content: message
   }
 
   request.send(JSON.stringify(params));
