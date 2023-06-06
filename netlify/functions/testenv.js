653e47e4d8777967e42ea7fd426070ecb7568bff
manolors <manolo.rs2005@gmail.com>
2023-06-06 17:39:42 +0100

    remove function

--- netlify/functions/testenv.js
+++
@@ -1,8 +0,0 @@
-
-exports.handler = async () => {
-  console.log("contenido de process.env" + JSON.stringify(process.env))
-  return {
-    statusCode: 200,
-    body: JSON.stringify({envVar:(process.env.MI_SECRET || "not-set")}),
-  };
-};
